6fb1807f9638227bb83d8af399654d3e3bcaa7f8
Liam Argent <liargent@hotmail.com>
2018-07-15 22:15:02 +0000

    Done.

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+var Tele = "Telecaster";
+var Strat = "Stratocaster"
+
+var guitar = "Potato";
+
+if (guitar === Strat) {
+  console.log("This guitar has three pickups and is a " + Strat + " electric guitar that is played by basically everybody.");
+}
+
+else if (guitar === Tele) {
+  console.log("This guitar has two pickups and is a " + Tele + " electric guitar that came out in the 50s probably.");
+}
+
+else {
+  console.log("Is it a guitar? Is it a plane? No idea.");
+}
